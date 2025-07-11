dd6c8b390e52761485811dfa9ed533ebd87e7523
whomstve123 <97992583+whomstve123@users.noreply.github.com>
2025-07-11 19:45:44 +0000

    more debug edits

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+console.log('BUILD DEBUG: SUPABASE_URL:', process.env.SUPABASE_URL)
+console.log('BUILD DEBUG: SUPABASE_ANON_KEY:', process.env.SUPABASE_ANON_KEY)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
