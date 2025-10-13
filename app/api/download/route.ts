721441276072787225857a5b033f309ffe876585
whomstve123 <97992583+whomstve123@users.noreply.github.com>
2025-10-13 18:12:02 +0000

    fixed type error

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -2,8 +2,11 @@ import { NextResponse } from "next/server"
 import crypto from "crypto"
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL || ""
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY || ""
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL or Key is missing from environment variables.")
+}
 const bucketName = "generated"
 
 const supabase = createClient(supabaseUrl, supabaseKey)
